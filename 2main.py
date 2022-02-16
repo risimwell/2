b270567307d18478a753b56aefe04e241afdcd0e
risimwell <risimwell@gmail.com>
2022-02-15 20:38:34 -0500

    comentario con el que se sube

---
+++ 2main.py
@@ -0,0 +1,11 @@
+suma = 0
+contador=0
+numero =0
+while numero < 10:
+    numero= int(input("Ingrese el numero"))
+    suma=  suma + numero
+contador+=1 
+promedio= suma/10
+print(promedio)"# ejercicio2" 
+"# ejercicio2" 
+"# ejercicio2" 
